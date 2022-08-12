b68b310f5bf6b99bdc5b274ad688f830fae29030
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-08-12 17:01:19 +0300

    upd

---
+++ src/pages/Login.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { logIn } from 'redux/auth/auth-operations';
+import { getIsLogged } from 'redux/auth/auth-selectors';
+import { Contacts } from './Contacts';
+
+import styles from './Login.module.css';
+
+export const Login = () => {
+  const isLogged = useSelector(getIsLogged);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleChange = evt => {
+    switch (evt.target.name) {
+      case 'email':
+        setEmail(evt.target.value);
+        break;
+      case 'password':
+        setPassword(evt.target.value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const handleLoginSubmit = async evt => {
+    evt.preventDefault();
+
+    const userToEnter = { email, password };
+
+    try {
+      await dispatch(logIn(userToEnter)).unwrap();
+      //   navigate('/contacts');
+    } catch (error) {
+      console.log('error');
+    }
+  };
+
+  return (
+    <>
+      {!isLogged ? (
+        <div className={styles.wrapperLogin}>
+          <form onSubmit={handleLoginSubmit}>
+            <input
+              onChange={handleChange}
+              name="email"
+              placeholder="Enter your email"
+              type="email"
+            />
+            <input
+              onChange={handleChange}
+              name="password"
+              placeholder="Enter your password"
+              type="password"
+            />
+            <button type="submit">Log in</button>
+          </form>
+        </div>
+      ) : (
+        <Contacts />
+        // <h2>Hello, go to your {<Link to="/contacts">Contacts</Link>}</h2>
+      )}
+    </>
+  );
+};
